a2f67b5d421c46cff638ebaa9d9c90c343ac02c3
q-13t <vova29092002@gmail.com>
2023-07-31 16:23:01 +0300

    Add project files.

---
+++ MainForm.cpp
@@ -0,0 +1,12 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Convo::MainForm mainForm;
+	Application::Run(% mainForm);
+
+}
